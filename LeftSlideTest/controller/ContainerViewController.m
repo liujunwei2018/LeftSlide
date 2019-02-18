691bf0547ade74f64cc6ec781f86a28273530c0a
刘君威 <>
2019-02-18 19:49:47 +0800

    添加注释

--- LeftSlideTest/controller/ContainerViewController.m
+++ LeftSlideTest/controller/ContainerViewController.m
@@ -17,14 +17,18 @@ CGFloat const kMaxSpeed = 800;
 @interface ContainerViewController ()
 @property (nonatomic, strong) MainViewController *mainVC;
 @property (nonatomic, strong) LeftViewController *leftVC;
-
+// 点击收起侧边栏
 @property (nonatomic, strong) UITapGestureRecognizer *tapGesture;
-@property (nonatomic, strong) UIScreenEdgePanGestureRecognizer *pan1;  // 开始的边缘平移手势
-@property (nonatomic, strong) UIPanGestureRecognizer *pan2; // 侧滑后的平移手势
+// 开始的边缘平移手势
+@property (nonatomic, strong) UIScreenEdgePanGestureRecognizer *pan1;
+// 侧滑后的平移手势
+@property (nonatomic, strong) UIPanGestureRecognizer *pan2;
 
 @end
 
 @implementation ContainerViewController
+
+#pragma mark - init
 
 + (instancetype)containerViewControllerWithLeftVC:(UIViewController *)leftVC mainVC:(UIViewController *)mainVC{
     return  [[ContainerViewController alloc] initWithLeftVC:leftVC mainVC:mainVC];
@@ -34,7 +38,7 @@ CGFloat const kMaxSpeed = 800;
     
     self = [super init];
     if (self) {
-        
+        // 根据自己创建的两个控制器进行相应的强制转换
         self.leftVC = (LeftViewController *)leftVC;
         self.mainVC = (MainViewController *)mainVC;
         
@@ -57,8 +61,9 @@ CGFloat const kMaxSpeed = 800;
 
 - (void)viewWillDisappear:(BOOL)animated {
     [super viewWillDisappear:animated];
+    // 显示导航栏,否则侧边栏中的跳转后的控制器没有导航栏
     [self.navigationController setNavigationBarHidden:NO animated:animated];
-    
+    // pop后返回的主页,不设置的话返回的是侧滑展示出来的状态
     [self hideLeftVc];
 }
 
@@ -67,13 +72,11 @@ CGFloat const kMaxSpeed = 800;
 - (void)setupUI {
     [self addChildViewController:self.mainVC];
     [self addChildViewController:self.leftVC];
-    [self.view addSubview:self.mainVC.view];
     [self.view addSubview:self.leftVC.view];
+    [self.view addSubview:self.mainVC.view];
     
     self.leftVC.view.frame = self.view.bounds;
     self.mainVC.view.frame = self.view.bounds;
-    // 将mainVC.view 放到最前面显示
-    [self.view bringSubviewToFront:self.mainVC.view];
     
     // 添加手势
     [self.mainVC.view addGestureRecognizer:self.pan1];
@@ -98,10 +101,12 @@ CGFloat const kMaxSpeed = 800;
     CGRect frame = self.mainVC.view.frame;
     frame.origin.x += point.x;
     self.mainVC.view.frame = frame;
-    // 边界限定
+    // 边界限定:
+    // 1.当滑动大于 kMaxLeftSlideWidth 时,设置为 kMaxLeftSlideWidth
     if (frame.origin.x >= kMaxLeftSlideWidth) {
         frame.origin.x = kMaxLeftSlideWidth;
     }
+    // 2.当滑动小于等于0时,设置为0
     if (frame.origin.x <= 0) {
         frame.origin.x = 0;
     }
@@ -109,8 +114,11 @@ CGFloat const kMaxSpeed = 800;
 
 
     if (pan.state == UIGestureRecognizerStateEnded) {
-        // 判断手势
+        /// 判断手势
+        // 屏幕边缘右滑
         if (pan == self.pan1) {
+            // 速度大于 kMaxSpeed 时自动展示完整
+            // 小于 kMaxSpeed 时为拖动效果 , 拖动超过 kScreenWidth / 2 时自动展示完全
             if (verPoint.x >= kMaxSpeed) {
                 [self showLeftVc];
             } else {
@@ -121,10 +129,13 @@ CGFloat const kMaxSpeed = 800;
                 }
             }
         }
+        // 左滑收回
         else {
+            // 左滑返回的 verPoint.x 是负数
             if (verPoint.x < -kMaxSpeed) {
                 [self hideLeftVc];
             } else {
+                // 左滑超过 kScreenWidth / 2 时自动收回
                 if (frame.origin.x >= kScreenWidth / 2) {
                     [self showLeftVc];
                 } else {
@@ -137,7 +148,6 @@ CGFloat const kMaxSpeed = 800;
     [pan setTranslation:CGPointZero inView:pan.view];
 }
 
-/// 方法2
 - (void)hideLeftVc{
  
     [UIView animateWithDuration:0.25 delay:0 options:UIViewAnimationOptionCurveEaseOut animations:^{
